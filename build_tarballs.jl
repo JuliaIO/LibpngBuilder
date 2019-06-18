54ef087a0bb4a4e9544ef37ee9d99a63dd4f7636
Simon <sdanisch@gmail.com>
2019-06-18 12:39:11 +0200

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -4,15 +4,15 @@ using BinaryBuilder
 
 # Collection of sources required to build libpng
 sources = [
-    "http://www.imagemagick.org/download/delegates/libpng-1.6.31.tar.gz" =>
-    "042e8701abc737a72f1393d0d8fca8ca86146460fe94c6c396261868f489c2de",
+    "https://vorboss.dl.sourceforge.net/project/libpng/libpng16/1.6.37/libpng-1.6.37.tar.gz" =>
+    "daeb2620d829575513e35fecc83f0d3791a620b9b93d800b763542ece9390fb4",
 
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd libpng-1.6.31/
+cd libpng-1.6.37/
 mkdir build
 cd build/
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain ..
@@ -42,9 +42,9 @@ products(prefix) = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.2/build_Zlib.v1.2.11.jl"
+    "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.4/build_Zlib.v1.2.11.jl"
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, "libpng", v"1.6.31", sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, "libpng", v"1.6.37", sources, script, platforms, products, dependencies)
 
